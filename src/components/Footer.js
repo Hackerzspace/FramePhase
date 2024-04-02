0f3f07907c1f458a1b91925910bfd3bd655b8af8
Parth Madhvani <madhvaniparth2@gmail.com>
2024-04-03 02:33:16 +0530

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from "next/link";
 import Image from 'next/image';
+import SparklesIcon from './SparklesIcon';
 
 const Footer = () => {
     return (
@@ -9,20 +10,23 @@ const Footer = () => {
             data-scroll-speed="-0.1">
 
             <div className='footer-content flex justify-around items-start mb-12 '>
-                <div className="content1 flex flex-col gap-4">
-                    <h3 className='text-xl text-semibold'>Product</h3>
-                    <Link href="/"><h3 className='text-slate-300'>Caption Generator</h3></Link>
-                    <Link href="/"><h3 className='text-slate-300'>Feedback</h3></Link>
-                </div>
+                <div className="content1 flex flex-col">
+                <Link href="/" className="flex gap-1 hover:text-slate-500">
+                    <SparklesIcon />
+                    <span>FramePhase</span>
+                </Link>
+            </div>
+            <div className='flex gap-20'>
                 <div className="content2 flex flex-col gap-4">
                     <h3 className='text-xl text-semibold'>Company</h3>
-                    <Link href="/pricing"><h3 className='text-slate-300'>Pricing</h3></Link>
-                    <Link href="https://github.com/Hackerzspace/FramePhase"><h3 className='text-slate-300'>About</h3></Link>
+                    <Link href="/pricing"><h3 className='text-slate-300 hover:text-slate-400'>Pricing</h3></Link>
+                    <Link href="https://github.com/Hackerzspace/FramePhase"><h3 className='text-slate-300 hover:text-slate-400'>About</h3></Link>
                 </div>
                 <div className="content3 flex flex-col gap-4">
                     <h3 className='text-xl text-semibold'>Support</h3>
-                    <Link href="mailto:contact@framephase.com"><h3 className='text-slate-300'>Contact Us</h3></Link>
+                    <Link href="mailto:contact@framephase.com"><h3 className='text-slate-300 hover:text-slate-400'>Contact Us</h3></Link>
                 </div>
+            </div>
 
             </div>
 
